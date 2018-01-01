ae44f688590fdc6e9f7cdf3a67f664fc8faf1dfc
fenghou <fenghou1st@gmail.com>
2018-01-01 18:10:25 +0800

    feat(*): add task summary

--- src/components/time-log/time-table/index.jsx
+++ src/components/time-log/time-table/index.jsx
@@ -73,21 +73,23 @@ class TimeTable extends Component {
       {
         begin: 1513863200000, // 2017-12-21 21:33 +0800
         end: 1513864200000, // 2017-12-21 21:50 +0800
-        name: 'test task 001',
-        project: 'test project 001',
+        name: 'test task 001 test task 001 test task 001 test task 001'
+          + ' test task 001 test task 001 test task 001 test task 001'
+          + ' test task 001 test task 001 test task 001 test task 001',
+        project: 'test project 001 test project 001',
         tags: ['test tag 001', 'test tag 002', 'test tag 003'],
       },
       {
         begin: 1513949600000, // 2017-12-22 21:33 +0800
         end: 1513950600000, // 2017-12-22 21:50 +0800
-        name: 'test task 002',
+        name: 'test task 002 test task 002 test task 002 test task 002',
         project: 'test project 001',
         tags: ['test tag 002', 'test tag 003', 'test tag 004'],
       },
       {
         begin: 1514036000000, // 2017-12-23 21:33:20 +0800
         end: 1514037000000, // 2017-12-23 21:50:00 +0800
-        name: 'test task 003',
+        name: 'test task 003 test task 003 test task 003 test task 003',
         project: 'test project 002',
         tags: ['test tag 003', 'test tag 004', 'test tag 005'],
       },
@@ -119,7 +121,6 @@ class TimeTable extends Component {
         taskId: null,
       },
     });
-    console.info(`onDayHover: ${date}`);
   }
 
   /**
@@ -140,7 +141,6 @@ class TimeTable extends Component {
         taskId: taskId,
       },
     });
-    console.info(`onSliceHover: ${sliceId}, ${taskId}`);
   }
 }
 
--- src/components/time-log/time-table/task-summary.jsx
+++ src/components/time-log/time-table/task-summary.jsx
@@ -14,7 +14,8 @@ class TaskSummary extends Component {
   constructor(props) {
     super(props);
 
-    this.DATE_FORMAT = 'MM-DD HH:mm';
+    this.DATE_FORMAT = 'MM-DD';
+    this.TIME_FORMAT = 'HH:mm';
   }
 
   /**
@@ -25,25 +26,34 @@ class TaskSummary extends Component {
       return (<div className={styles.summary}/>);
     }
 
-    const beginTime = new Date(this.props.task.begin);
-    const endTime = new Date(this.props.task.end);
+    const beginTime = moment(new Date(this.props.task.begin));
+    const endTime = moment(new Date(this.props.task.end));
 
     return (
         <div className={styles.summary}>
           <span className={styles.time}>
             <span className={styles.timeBegin}>
-              {moment(beginTime).format(this.DATE_FORMAT)}
+              <span className={styles.timeDate}>
+                {beginTime.format(this.DATE_FORMAT)}
+              </span>
+              <span className={styles.timeTime}>
+                {beginTime.format(this.TIME_FORMAT)}
+              </span>
             </span>
-            <span className={styles.timeSeparator}>-</span>
             <span className={styles.timeEnd}>
-              {moment(endTime).format(this.DATE_FORMAT)}
+              <span className={styles.timeDate}>
+                {endTime.format(this.DATE_FORMAT)}
+              </span>
+              <span className={styles.timeTime}>
+                {endTime.format(this.TIME_FORMAT)}
+              </span>
             </span>
-          </span>
-          <span className={styles.name}>
-            {this.props.task.name}
           </span>
           <span className={styles.project}>
             {this.props.task.project}
+          </span>
+          <span className={styles.name}>
+            {this.props.task.name}
           </span>
         </div>
     );
